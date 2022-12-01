6152a9415b3ce35b4becbd3f8023cb5e9b2f501f
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-12-01 23:13:51 +0100

    add: search bar, chiamata axios, store

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from 'axios'
+import { store } from "./store"
+
+import AppHeader from './components/AppHeader.vue';
 export default {
   components: {
-    HelloWorld,
+    AppHeader,
   },
-};
+  data(){
+    return{
+      store,
+    }
+  },
+  methods:{
+    getData(){
+      axios.get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: 'e1a9bf61d337fd6893f6391c4a061bc8',
+          query: this.store.TextToSearch,
+          language: 'it-IT',
+        },
+      }).then((response)=>{
+        this.store.movies = response.data.results
+      })
+    }
+  },
+}
 </script>
 
 <template>
   <div>
-    <HelloWorld />
+    <header>
+      <AppHeader
+      class="p-2"
+      @performSearch="getData"/>
+    </header>
   </div>
 </template>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+import { store } from "../store";
+    export default {
+        data(){
+            return{
+                store,
+            }
+        }
+    }
+</script>
+
+<template>
+    <form @submit.prevent="$emit('performSearch')">
+        <input type="text" placeholder="fai una ricerca"  v-model="store.TextToSearch" required>
+        <button type="submit">Cerca</button>
+    </form>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
